eef669dc9e1985adbb7f5633be040398f2e847e9
AnnaKlim69 <a_f_klim@mail.ru>
2024-03-20 11:05:45 +0300

    Edit4 Added checks Header and HotelsPage

--- src/test/java/booking/pageObject/baseComponents/Header.java
+++ src/test/java/booking/pageObject/baseComponents/Header.java
@@ -14,39 +14,31 @@ import static framework.Browser.getDriver;
 
 public class Header {
     private static final String HEADER_LOGO = "//a[@aria-label='Booking.com']";
+    private static final String CURRENCY_ITEM = "//span[text()='Доллар США']";
+    private static final String LANGUAGE_ITEM = "//span[@class='cf67405157' and text()='English (US)']";
     protected static final Button HEADER_LANGUAGE = new Button(By.xpath("//button[@data-testid='header-language-picker-trigger']"));
     protected static final Button HEADER_CURRENCY = new Button(By.xpath("//button[@data-testid='header-currency-picker-trigger']"));
     protected static final Button HEADER_REGISTER = new Button(By.xpath("//a[@data-testid='header-sign-up-button']"));
     protected static final Button HEADER_SING_IN = new Button(By.xpath("//a[@data-testid='header-sign-in-button']"));
-    private static final Button CURRENCY_ITEM = new Button(By.xpath("//span[text()='Доллар США']"));
-    private static final Button LANGUAGE_ITEM = new Button(By.xpath("//span[@class='cf67405157' and text()='%s']"));
-    private static final List<String> LANGUAGE_LIST = getDriver().findElements(By.xpath
-                    ("//div[@class='eb33ef7c47']")).stream().map(e -> e.getText())
-            .collect(Collectors.toList());
-    private static final List<String> CURRENCY_LIST = getDriver().findElements(By.xpath
-                    ("///div[@class='c0528ecc22']")).stream().map(e -> e.getText())
-            .collect(Collectors.toList());
 
-    private WebElement languageItem;
     public static String languageName;
-    private WebElement currencyItem;
     public static String currencyName;
     private static String item;
+
+    public String getLanguageName() {
+        languageName = LANGUAGE_ITEM;
+        return languageName;
+    }
+
+    public String getCurrency() {
+        currencyName = CURRENCY_ITEM;
+        return currencyName;
+    }
 
     @Step("Click Header Logo")
     public void clickHeaderLogo() {
         Label headerLogo = new Label(By.xpath(String.format(HEADER_LOGO, item)));
         headerLogo.click();
-    }
-
-    @Step("Click Button Language")
-    public void clickButtonLanguage() {
-        HEADER_LANGUAGE.click();
-    }
-
-    @Step("Click Language")
-    public void clickLanguage() {
-        LANGUAGE_ITEM.click();
     }
 
     @Step("Click button currency")
@@ -56,7 +48,19 @@ public class Header {
 
     @Step("Click currency")
     public void clickCurrency() {
-        CURRENCY_ITEM.click();
+        Label currencyItem = new Label(By.xpath(String.format(CURRENCY_ITEM, item)));
+        currencyItem.click();
+    }
+
+    @Step("Click Button Language")
+    public void clickButtonLanguage() {
+        HEADER_LANGUAGE.click();
+    }
+
+    @Step("Click Language")
+    public void clickLanguage() {
+        Label languageItem = new Label(By.xpath(String.format(LANGUAGE_ITEM, item)));
+        languageItem.click();
     }
 
     @Step("Click button register")
@@ -69,42 +73,6 @@ public class Header {
         HEADER_SING_IN.click();
     }
 
-    @Step("Language selection")
-    public WebElement languageSelection() {
-        Random random = new Random();
-        int r = random.nextInt(LANGUAGE_LIST.size());
-        String languageName = LANGUAGE_LIST.get(r);
-        System.out.println(languageName);
-        return languageItem = getDriver().findElement(By.xpath(String.format(languageName, LANGUAGE_ITEM)));
-    }
-
-    @Step("Item validation language")
-    public void itemValidationLanguage() {
-        if (languageItem.equals(languageName)) {
-            System.out.println("Items are equal");
-        } else {
-            System.out.println("Items are not equal");
-        }
-    }
-
-    @Step("Currency selection")
-    public WebElement currencySelection() {
-        Random random = new Random();
-        int r = random.nextInt(CURRENCY_LIST.size());
-        String currencyName = CURRENCY_LIST.get(r);
-        System.out.println(currencyName);
-        return currencyItem = getDriver().findElement(By.xpath(String.format(currencyName, CURRENCY_LIST)));
-    }
-
-    @Step("Item validation currency")
-    public void itemValidationCurrency() {
-        if (currencyItem.equals(currencyName)) {
-            System.out.println("Items are equal");
-        } else {
-            System.out.println("Items are not equal");
-        }
-    }
-
     @Step("Get button register item")
     public void getButtonRegisterItem() {
         Label buttonRegisterItem = new Label(By.xpath(String.format(item, HEADER_REGISTER)));
@@ -115,5 +83,23 @@ public class Header {
     public void getButtonSingInItem() {
         Label buttonRegisterItem = new Label(By.xpath(String.format(item, HEADER_SING_IN)));
         buttonRegisterItem.click();
+    }
+
+    @Step("Item validation language")
+    public void itemValidationLanguage() {
+        if (item.equals(languageName)) {
+            System.out.println("Items are equal");
+        } else {
+            System.out.println("Items are not equal");
+        }
+    }
+
+    @Step("Item validation currency")
+    public void itemValidationCurrency() {
+        if (item.equals(currencyName)) {
+            System.out.println("Items are equal");
+        } else {
+            System.out.println("Items are not equal");
+        }
     }
 }
